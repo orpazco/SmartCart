4459a53ce1c8aa32711ff28bade81d5d831986a3
Orpaz Cohen <orpaz@avalor.io>
2022-12-02 20:15:12 +0200

    update main

--- src/main/main.py
+++ src/main/main.py
@@ -2,5 +2,5 @@ from api import app
 from db import db_session
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=False, port=6000, host='127.0.0.1')
     db_session.close()
