6c228cdf93c7f8b3d680f28840a09632531cb57d
Orpaz Cohen <orpaz@avalor.io>
2022-12-02 14:58:42 +0200

    fix queries in db

--- db.py
+++ db.py
@@ -6,14 +6,15 @@ class SmartCartDB:
     def __init__(self):
         self.con = sqlite3.connect("smart-cart.db", check_same_thread=False)
         self.cursor = self.con.cursor()
+        self._next_pid = 0
         self._create_tables()
         self._update_products()
 
     def add_new_product(self, product_name: str, cost: float):
-        pid = str(uuid.uuid1())
-        self.cursor.execute("""INSERT INTO groceries(product_id,product_name,cost)
-                                VALUES(?,?,?)""", (pid, product_name, cost))
+        self.cursor.execute("""INSERT OR IGNORE INTO groceries(product_id,product_name,cost)
+                                VALUES(?,?,?)""", (self._next_pid, product_name, cost))
         self.con.commit()
+        self._next_pid += 1
 
     def add_new_customer(self, cid: str, name: str, active: bool):
         self.cursor.execute("""INSERT INTO customers(id,name,active) VALUES(?,?,?)""", (cid, name, int(active)))
@@ -24,26 +25,30 @@ class SmartCartDB:
         self.con.commit()
 
     def get_cart(self, cid):
-        self.cursor.execute("""SELECT groceries.product_name
-                            FROM groceries 
-                            WHERE carts.customer_id=?
-                            INNER JOIN carts ON carts.product_id=groceries.product_id;""", cid)
+        self.cursor.execute(f"""SELECT groceries.product_name, groceries.cost
+                                            FROM groceries
+                                            INNER JOIN carts ON carts.product_id=groceries.product_id
+                                            WHERE carts.customer_id=?;""", (str(cid),))
         return self.cursor.fetchall()
+
+    def get_cost(self, product_name):
+        self.cursor.execute("""SELECT cost
+        FROM groceries
+        """)
 
     def _create_tables(self):
         self.cursor.execute("""CREATE TABLE IF NOT EXISTS customers (
                                         id text PRIMARY KEY,
-                                        name text NOT NULL,
-                                        active integer NOT NULL
+                                        name text NOT NULL
                                     );""")
         self.cursor.execute("""CREATE TABLE IF NOT EXISTS carts (
-                                        customer_id integer,
+                                        customer_id text,
                                         product_id integer NOT NULL,
                                         FOREIGN KEY (customer_id) REFERENCES customers (id),
                                         FOREIGN KEY (product_id) REFERENCES groceries (product_id)
                                     );""")
         self.cursor.execute("""CREATE TABLE IF NOT EXISTS groceries (
-                                        product_id text PRIMARY KEY,
+                                        product_id integer PRIMARY KEY,
                                         product_name text NOT NULL,
                                         cost real NOT NULL
                                     );""")
